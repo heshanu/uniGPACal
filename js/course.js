f4259a7b5d6eeaecd4d1318624b7a72aeb2660df
heshanu <heshanu97@gmail.com>
2024-06-30 11:52:59 +0530

    modify minior changes

---
+++ js/course.js
@@ -0,0 +1,336 @@
+/* 
+
+GPA Calculator by Nuwan94
+https://github.com/nuwan94/se-gpa-calculator
+
+*/
+
+
+var courseName = "B Tech in Software Technology - University of Vocational Technology";
+
+var courseUnits = [
+    {
+        year: '1',
+        sems: [
+            {
+                sem: '1',
+                subs: [{
+                    name: 'Mathematics for ICT 1',
+                    id: 'IT103011'
+                }, {
+                    name: 'Computer Programming',
+                    id: 'IT104021'
+                }, {
+                    name: 'Software Development Practices',
+                    id: 'IT104031'
+                }, {
+                    name: 'Data Communication and Network',
+                    id: 'IT104051'
+                }, {
+                    name: 'Digital Eletronics',
+                    id: 'IT104041'
+                }, {
+                    name: 'Database Design',
+                    id: 'IT104061'
+                },
+                {
+                    name: 'Internet Techonlogy',
+                    id: 'IT104071'
+                },
+                    // {
+                    //         name: 'Database Design',
+                    //         id: 'IT104061'
+                    // },
+                ]
+            },
+            {
+                sem: '2',
+                subs: [{
+                    name: 'Operation Systems',
+                    id: 'IT202011'
+                }, {
+                    name: 'Mathematics for ICT 11',
+                    id: 'IT203021'
+                }, {
+                    name: 'Computer Architecture',
+                    id: 'IT203031'
+                }, {
+                    name: 'Data Struture and Algorithms',
+                    id: 'IT204041'
+                }, {
+                    name: 'Database and programming',
+                    id: 'IT206051'
+                }, {
+                    name: 'Visual Programming 1',
+                    id: 'IT204061'
+                },
+                {
+                    name: 'Web Programming',
+                    id: 'IT206071'
+                },
+                {
+                    name: 'Computer Network',
+                    id: 'IT203081'
+                },
+                    // {
+                    //     name: 'Visual Programming',
+                    //     id: 'IT203091'
+                    // },
+                ]
+            }
+        ]
+    },
+    {
+        year: '2',
+        sems: [
+            {
+                sem: '3',
+                subs: [
+                    {
+                        name: 'Web Technology and Application',
+                        id: 'IT304011'
+                    }, {
+                        name: 'PIP',
+                        id: 'IT304021'
+                    }, {
+                        name: 'Visual Programming 11',
+                        id: 'IT306031'
+                    }, {
+                        name: 'Software Arch and Design',
+                        id: 'IT306041'
+                    },
+                    {
+                        name: 'Software Deployment and Evolution',
+                        id: 'IT306051'
+                    }
+                    , {
+                        name: 'Database Implmentation',
+                        id: 'IT304061'
+                    }, {
+                        name: 'Entrepreurship Development and Management',
+                        id: 'MS304121'
+                    }
+                    //, {
+                    //     id: "IT302160",
+                    //     name: "Professional Practices"
+                    // }, {
+                    //     name: 'Interactive Application Development',
+                    //     id: 'SENG 21263',
+                    //     type: 'o'
+                    //     }
+                ]
+            },
+            {
+                sem: '4',
+                subs: [{
+                    name: 'Software Architecture and Design',
+                    id: 'SENG 22212'
+                }, {
+                    name: 'Human Computer Interaction',
+                    id: 'SENG 22223'
+                }, {
+                    name: 'Software Verification and Validation',
+                    id: 'SENG 22233'
+                }, {
+                    name: 'Mobile Application Development',
+                    id: 'SENG 22243'
+                }, {
+                    name: 'Computer Networks',
+                    id: 'SENG 24213'
+                }, {
+                    name: 'Embedded Systems Development',
+                    id: 'SENG 22253',
+                    type: 'o'
+                }, {
+                    name: 'Mathematical Methods',
+                    id: 'PMAT 22213',
+                    type: 'o'
+                }]
+            }
+        ]
+    },
+    {
+        year: '3',
+        sems: [{
+            sem: '5',
+            subs: [{
+                id: "SENG 31212",
+                name: "Software Quality"
+            }, {
+                id: "SENG 31222",
+                name: "Information Security"
+            }, {
+                id: "SENG 31232",
+                name: "Software Project Management"
+            }, {
+                id: "SENG 31242",
+                name: "System Design Project"
+            }, {
+                id: "SENG 31262",
+                name: "Research Methods"
+            }, {
+                id: "SENG 31282",
+                name: "Computer Network Management",
+                type: 'o'
+            },
+
+            // Not Available for 2015/16
+            {
+                id: "SENG 31272",
+                name: "Internet of Things",
+                type: 'o'
+            }, {
+                id: "SENG 31292",
+                name: "Enterprise Information Systems",
+                type: 'o'
+            },
+            {
+                id: "SENG 34222",
+                name: "Software Process"
+            }, {
+                id: "SENG 31313",
+                name: "Advanced Web Applications Development",
+                type: 'n'
+            }, {
+                id: "SENG 31323",
+                name: "Mobile Computing Technologies",
+                type: 'm'
+            }, {
+                id: "SENG 31333",
+                name: "Business Intelligence and Management Support Systems",
+                type: 'd'
+            }, {
+                id: "SENG 31343",
+                name: "Health Information Management",
+                type: 'h'
+            }, {
+                id: "SENG 31353",
+                name: "Game Development Technology",
+                type: 'g'
+            }, {
+                id: "SENG 31363",
+                name: "Business Systems Modelling and Optimization",
+                type: 'b'
+            }
+            ]
+        },
+        {
+            sem: '6',
+            subs: [{
+                id: "SENG 34213",
+                name: "Systems Development Project"
+            }, {
+                id: "SENG 32216",
+                name: "Internship"
+            }]
+        }
+        ]
+    }, {
+        year: '4',
+        sems: [{
+            sem: '7',
+            subs: [{
+                name: 'Software Evolution',
+                id: 'SENG 41212'
+            }, {
+                name: 'Software Metrics and Measurements',
+                id: 'SENG 41222'
+            }, {
+                name: 'Digital Image Processing',
+                id: 'SENG 41233',
+                type: 'o'
+            }, {
+                name: 'Advanced Databases',
+                id: 'SENG 41242',
+                type: 'o'
+            }, {
+                name: 'Advanced Computer Networks',
+                id: 'SENG 41252',
+                type: 'o'
+            }, {
+                name: 'Speech Interfaces',
+                id: 'SENG 41262',
+                type: 'o'
+            }, {
+                name: 'Formal Methods',
+                id: 'SENG 41272',
+                type: 'o'
+            }, {
+                id: "SENG 41283",
+                name: "Distributed and Cloud Computing",
+                type: 'n'
+            }, {
+                id: "SENG 41293",
+                name: "Mobile Web Application Development",
+                type: 'm'
+            }, {
+                id: "SENG 41303",
+                name: "Big Data Infrastructure",
+                type: 'd'
+            }, {
+                id: "SENG 41313",
+                name: "Health Information Systems Design and Development",
+                type: 'h'
+            }, {
+                id: "SENG 41323",
+                name: "Games Design, Artwork, and Programming",
+                type: 'g'
+            }, {
+                id: "SENG 41333",
+                name: "Computer Based Operations Management",
+                type: 'b'
+            }]
+        },
+        {
+            sem: '8',
+            subs: [{
+                name: 'Software Safety and Reliability',
+                id: 'SENG 42212'
+            }, {
+                name: 'Software Engineering Research Project',
+                id: 'SENG 43216'
+            }, {
+                name: 'Usability Engineering',
+                id: 'SENG 42222',
+                type: 'o'
+            }, {
+                name: 'Software Management',
+                id: 'SENG 42232',
+                type: 'o'
+            }, {
+                name: 'Machine Learning',
+                id: 'SENG 42242',
+                type: 'o'
+            }, {
+                name: 'Computer Graphics',
+                id: 'SENG 42252',
+                type: 'o'
+            }, {
+                id: "SENG 42273",
+                name: "Semantic Web and Ontological Engineering",
+                type: 'n'
+            }, {
+                id: "SENG 42283",
+                name: "Mobile Networks",
+                type: 'm'
+            }, {
+                id: "SENG 42293",
+                name: "Big Data Analytics",
+                type: 'd'
+            }, {
+                id: "SENG 42303",
+                name: "Medical Imaging and Biomedical Signal Processing",
+                type: 'h'
+            }, {
+                id: "SENG 42313",
+                name: "Advanced Topics in Game Design and Animation",
+                type: 'g'
+            }, {
+                id: "SENG 42323",
+                name: "Business Process Engineering",
+                type: 'b'
+            }]
+        }
+        ]
+    }
+];
